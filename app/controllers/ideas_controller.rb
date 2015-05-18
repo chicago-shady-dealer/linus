7274416bab31ed1b76c9eb9038637a9f2ce348ca
Jeremy Archer <open-source@fatlotus.com>
2015-05-18 09:33:58 -0500

    Disable feature that was overwriting issue IDs.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -82,7 +82,7 @@ class IdeasController < ApplicationController
   # POST /ideas.json
   def create
     @idea = Idea.new(params[:idea].slice(:headline, :issue_id))
-    @idea.issue = Issue.choose_issue(nil)
+    @idea.issue ||= Issue.choose_issue(nil)
 
     respond_to do |format|
       if @idea.save
